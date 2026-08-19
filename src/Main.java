e099e105144beec0ab4dcccdc50e8fdb6224c3ab
patis <icloud.imail@icloud.com>
2026-08-20 00:14:17 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import ru.aston.hometask01.Mutable;
+import ru.aston.hometask01.Immutable;
+import ru.aston.hometask02.Book;
 
 void main() {
+    // Task 01
+//    Mutable mutable = null;
+//    Immutable immutable = new Immutable("Bob", mutable);
+//    System.out.println("Создаем классы:");
+//    System.out.println(mutable);
+//    System.out.println(immutable);
+//    System.out.println();
+//    mutable = new Mutable(50);
+//
+//    System.out.println("Меняем мутабельный класс, который используется в имутабельном");
+//    mutable.setWeight(70);
+//    System.out.println();
+//
+//    System.out.println("Проверяем изменения в классах:");
+//    System.out.println(mutable);
+//    System.out.println(immutable);
+
 
 }
